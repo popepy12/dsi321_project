d763eccb6e8cc5818ab47a719591860c7dfd7a60
natjirach <104311311+natjira-chamusri@users.noreply.github.com>
2022-05-11 20:55:18 +0700

    Delete CIS add sign up Copy directory

--- CIS add sign up Copy/website/models.py
+++
@@ -1,8 +0,0 @@
-from . import db
-from flask_login import UserMixin
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    studentid = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-
